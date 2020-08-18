ea8d09935d9914dd17ec47a580d71c5b51921c30
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-08-18 13:16:28 +0530

    Added the fragments for bottom navigation bar

---
+++ app/src/main/java/com/example/instagramclone/Fragments/NotificationsFragment.java
@@ -0,0 +1,22 @@
+package com.example.instagramclone.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.instagramclone.R;
+
+
+public class NotificationsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+}
