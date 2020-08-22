5f3acd09bc3bc7f1eb4a474b62f7eca80c1c6088
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-08-22 15:08:45 +0530

    Layout for the Login Activity

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.instagramclone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
       private Button login;
@@ -14,26 +16,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login = findViewById(R.id.btn_login);
         register = findViewById(R.id.btn_register);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent login_intent = new Intent(MainActivity.this, LoginActivity.class);
-                login_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(login_intent);
 
-            }
-        });
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent register_intent = new Intent(MainActivity.this , RegisterActivity.class);
+                Intent register_intent = new Intent(MainActivity.this, RegisterActivity.class);
                 register_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(register_intent);
             }
         });
+    }
+
+    public void callLoginScreen(View view) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.btn_login), "transition_login");
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+        startActivity(intent, options.toBundle());
     }
 }
